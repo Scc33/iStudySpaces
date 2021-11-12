8259ca8dd1585498318e97f3cce33b0388bcb1ce
Sean Coughlin <seanmc4@illinois.edu>
2021-11-12 11:41:45 -0600

    Link filter and layout with z

--- iStudySpace/app/src/main/java/com/example/istudyspace/FilterActivity.java
+++ iStudySpace/app/src/main/java/com/example/istudyspace/FilterActivity.java
@@ -1,14 +1,29 @@
 package com.example.istudyspace;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class FilterActivity extends AppCompatActivity {
+public class FilterActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button applyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
+
+        applyButton = (Button) findViewById(R.id.apply);
+
+        applyButton.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        if (v.getId() == R.id.apply) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
