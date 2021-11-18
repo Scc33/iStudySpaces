4f27cc3b724673bc1d82529625964b570364898a
Sean Coughlin <seanmc4@illinois.edu>
2021-11-17 18:20:05 -0600

    Update noise/interaction level selection

--- iStudySpace/app/src/main/java/com/example/istudyspace/FilterActivity.java
+++ iStudySpace/app/src/main/java/com/example/istudyspace/FilterActivity.java
@@ -5,16 +5,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButtonToggleGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FilterActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button anyButton;
-    private Button quietButton;
-    private Button ambientButton;
-    private Button loudButton;
     private CheckBox groupButton;
     private CheckBox coffeeButton;
     private CheckBox foodButton;
@@ -41,34 +38,16 @@ public class FilterActivity extends AppCompatActivity implements View.OnClickLis
         }
         setContentView(R.layout.activity_filter);
 
-        toggleGroup = (MaterialButtonToggleGroup) findViewById(R.id.toggleButton);
         applyButton = (Button) findViewById(R.id.apply);
-        anyButton = (Button) findViewById(R.id.anyButton);
-        quietButton = (Button) findViewById(R.id.quietButton);
-        ambientButton = (Button) findViewById(R.id.ambientButton);
-        loudButton = (Button) findViewById(R.id.loudButton);
         groupButton = (CheckBox) findViewById(R.id.groupCheck);
         coffeeButton = (CheckBox) findViewById(R.id.coffeeCheck);
         foodButton = (CheckBox) findViewById(R.id.foodCheck);
 
-        if (noiseLevel.equals("any")) {
-            toggleGroup.check(R.id.anyButton);
-        } else if (noiseLevel.equals("quiet")) {
-            toggleGroup.check(R.id.quietButton);
-        } else if (noiseLevel.equals("ambient")) {
-            toggleGroup.check(R.id.ambientButton);
-        } else {
-            toggleGroup.check(R.id.loudButton);
-        }
         groupButton.setChecked(groupWork);
         coffeeButton.setChecked(coffee);
         foodButton.setChecked(food);
 
         applyButton.setOnClickListener(this);
-        anyButton.setOnClickListener(this);
-        quietButton.setOnClickListener(this);
-        ambientButton.setOnClickListener(this);
-        loudButton.setOnClickListener(this);
         groupButton.setOnClickListener(this);
         coffeeButton.setOnClickListener(this);
         foodButton.setOnClickListener(this);
@@ -83,14 +62,6 @@ public class FilterActivity extends AppCompatActivity implements View.OnClickLis
             intent.putExtra("coffee", coffee);
             intent.putExtra("food", food);
             startActivity(intent);
-        } else if (v.getId() == R.id.anyButton) {
-            noiseLevel = "any";
-        } else if (v.getId() == R.id.quietButton) {
-            noiseLevel = "quiet";
-        } else if (v.getId() == R.id.ambientButton) {
-            noiseLevel = "ambient";
-        } else if (v.getId() == R.id.loudButton) {
-            noiseLevel = "loud";
         } else if (v.getId() == R.id.groupCheck) {
             groupWork = !groupWork;
         } else if (v.getId() == R.id.coffeeCheck) {
